f3cd531db3e9dd16a37fa43300101c697cc024a7
Pratik Sunil Bhujbal <pbhujbal@umd.edu>
2022-08-26 14:03:24 -0400

    updated for initial variables

--- code/main.m
+++ code/main.m
@@ -4,8 +4,9 @@
 % This script is to help run your algorithm and visualize the result from it.
 
 %% Load data
-clear all;
+clear;
 close all;
+clc;
 
 load practice.mat
 % This will load four variables: ranges, scanAngles, t, pose
@@ -18,7 +19,7 @@ load practice.mat
 %     angles are with respect to the body coordinate frame.
 % [4] M is a 2D array containing the occupancy grid map of the location
 %     e.g. map(x,y) is a log odds ratio of occupancy probability at (x,y)
-
+load practice-answer.mat;
 %% Set parameters
 param = {};
 % 1. Decide map resolution, i.e., the number of grids for 1 meter.
@@ -29,42 +30,21 @@ param.origin = [685,572]';
 
 param.init_pose = -init_pose;
 
-%% Plot LIDAR data
-lidar_local = [ranges(:,1).*cos(scanAngles) -ranges(:,1).*sin(scanAngles)];
+start = 1;
+N = length(t)-1;
+param.init_pose = pose(:,start);
 
+% Adding actual data in param object to plot
+param.pose = pose;
+param.t = t;
 
-figure,
-plot(0,0,'rs'); hold on;
-plot(lidar_local(:,1),lidar_local(:,2),'.-'); 
-axis equal;
-set(gca,'YDir','reverse');
-xlabel('x');
-ylabel('y');
-grid on;
-title('Lidar measurement in the body frame');
 
 %% Run algorithm
 % Call your mapping function here.
 % Running time could take long depending on the efficiency of your code.
 % For a quicker test, you may take some hundreds frames as input arguments as
 % shown.
-%pose = particleLocalization(ranges(:,1:1000), scanAngles, M, param);
-load practice-answer.mat;
 
-%% Plot final solution
-% The final grid map:
-figure;
-imagesc(M); hold on;
-
-%% Plot LIDAR data
-lidar_global(:,1) =  (ranges(:,1).*cos(scanAngles + pose(3,1)) + pose(1,1))*param.resol + param.origin(1);
-lidar_global(:,2) = (-ranges(:,1).*sin(scanAngles + pose(3,1)) + pose(2,1))*param.resol + param.origin(2);
-
-plot(lidar_global(:,1), lidar_global(:,2), 'g.'); 
-
-
-colormap('gray');
-axis equal;
-hold on;
-plot(pose(1,:)*param.resol+param.origin(1), ...
-    pose(2,:)*param.resol+param.origin(2), 'r.-');
+pose_calc = particleLocalization(ranges(:,start:start+N), scanAngles, M, param);
+pose = pose(:,start:start+N);
+ranges = ranges(:,start:start+N);
