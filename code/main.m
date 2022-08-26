649cde86f2c012733ef74b841f3e0878fd7aa1c5
Pratik Sunil Bhujbal <pbhujbal@umd.edu>
2022-08-26 14:15:15 -0400

    updated file structure

--- code/main.m
+++ code/main.m
@@ -8,7 +8,7 @@ clear;
 close all;
 clc;
 
-load practice.mat
+load ../data_files/practice.mat
 % This will load four variables: ranges, scanAngles, t, pose
 % [1] t is K-by-1 array containing time in second. (K=3701)
 %     You may not need to use time info for implementation.
@@ -19,7 +19,7 @@ load practice.mat
 %     angles are with respect to the body coordinate frame.
 % [4] M is a 2D array containing the occupancy grid map of the location
 %     e.g. map(x,y) is a log odds ratio of occupancy probability at (x,y)
-load practice-answer.mat;
+load ../data_files/practice-answer.mat;
 %% Set parameters
 param = {};
 % 1. Decide map resolution, i.e., the number of grids for 1 meter.
